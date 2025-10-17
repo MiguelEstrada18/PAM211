5ed976a6d26de1830501d6b016229a4ae5643452
MiguelEstrada18 <124051501@upq.edu.mx>
2025-10-16 20:34:18 -0600

    Buttons y Switch Completada
    La práctica de Buttons y Switch fue terminada en tiempo y forma.

--- INTRO/screens/BotonesScreen.js
+++ INTRO/screens/BotonesScreen.js
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   botonApagar:{
-    backgroundColor:'red',
+    backgroundColor:'orange',
     paddingVertical: 10,
     paddingHorizontal: 25,
     borderRadius: 8,
