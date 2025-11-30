98aa4b1a74de1814be1e0528731ec61e8829728d
MiguelEstrada18 <124051501@upq.edu.mx>
2025-11-30 13:11:28 -0600

    Practica: Modal Completa
    La práctica del Modal fue finalizada

--- INTRO/screens/ModalScreen.js
+++ INTRO/screens/ModalScreen.js
@@ -1,10 +1,53 @@
-import { Text, View } from 'react-native'
-
+import { Text, View, StyleSheet, Button, Modal } from 'react-native'
+import React, {useState} from 'react';
 
 export default function ModalScreen() {
-    return (
-      <View>
-        <Text>Proximamente.....</Text>
-      </View>
-    )
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return(
+    <View style={styles.container}>
+      <Button title='Mostrar Modal' onPress={()=> setModalVisible(true)} color='gray'>
+        Da Click
+      </Button>
+
+      <Modal animationType='fade' transparent={true} visible={modalVisible} onRequestClose={()=> setModalVisible(!modalVisible)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.textoModal}>
+              Esto es un Modal
+            </Text>
+            <Button title='Ocultar Modal' onPress={()=> setModalVisible(false)} color='gray'>
+            </Button>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000ff',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'powderblue',
+  },
+  modalContent: {
+    backgroundColor: '#ffffff',
+    padding: 25,
+    borderRadius: 15,
+    alignItems: 'center',
+  },
+  textoModal: {
+    color: '#000000',
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+});
